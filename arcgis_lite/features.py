e83823205d6b409b2c49a091d668228a1555965a
J. R. Matchett <6579155+jrmatchett@users.noreply.github.com>
2021-10-08 21:24:52 -0700

    AGOL and Portal GIS classes

--- arcgis_lite/features.py
+++ arcgis_lite/features.py
@@ -1,27 +1,31 @@
 import requests
-from gis import PortalGIS
 
 
 class FeatureLayer:
     '''ArcGIS Feature Layer'''
-    def __init__(self, url: str, gis: PortalGIS=None) -> None:
+    def __init__(self, url, gis=None):
         self.url = url
         self.gis = gis
 
-    def query(self, where: str='1=1', outFields: str='*', returnGeometry: bool=True, outSR: int=4326) -> list:
+    def query(self, where: str='1=1', outFields: str='*', returnGeometry: bool=True, **kwargs):
         '''Query features'''
-        query_params = {k: v for k,v in locals().items() if k != 'self'}
+        query_params = {k:v for k,v in locals().items() if k not in ['self', 'kwargs']}
+        if kwargs:
+            query_params.update(kwargs)
         query_params['f'] = 'json'
         query_params['token'] = self.gis.token if self.gis else None
+        #print(query_params, flush=True)
         return self._paged_query(query_params, [])
 
     def _paged_query(self, query_params, features):
         query_params['resultOffset'] = len(features)
-        print(f"query rec start: {query_params['resultOffset']}", flush=True)
+        #print(f"query rec start: {query_params['resultOffset']}", flush=True)
         query_data = requests.get(
             self.url + '/query',
             params=query_params
         ).json()
+        if not 'features' in query_data:
+            return query_data
         features.extend(query_data['features'])
         if 'exceededTransferLimit' in query_data and query_data['exceededTransferLimit']:
             self._paged_query(query_params, features)
--- arcgis_lite/gis.py
+++ arcgis_lite/gis.py
@@ -2,32 +2,68 @@ import datetime
 import requests
 
 
-class PortalGIS:
-    '''Connection to an ArcGIS Enterprise Portal'''
-    def __init__(self, url: str, client_id: str, refresh_token: str) -> None:
+class _GIS:
+    '''Abstract GIS superclass'''
+    def __init__(self, url):
         self.url = url.strip('/')
         self.rest_url = url + '/sharing/rest'
-        self.client_id = client_id
-        self.refresh_token = refresh_token
         self._token = None
         self.token_expiration = None
 
-    def _get_token(self) -> None:
+    def _get_token(self):
+        # implemented by subclasses
+        pass
+
+    @property
+    def token(self):
+        '''GIS access token'''
+        self._get_token()
+        return self._token
+
+
+class AgolGIS(_GIS):
+    '''Connection to ArcGIS Online'''
+    def __init__(self, username, password, url='https://www.arcgis.com'):
+        super().__init__(url)
+        self.username = username
+        self.password = password
+
+    def _get_token(self):
         if not self._token or \
-            datetime.datetime.now() - self.token_expiration > datetime.timedelta(minutes=28):
+            datetime.datetime.utcnow() > self.token_expiration - datetime.timedelta(minutes=2):
+            token_data = requests.post(
+                self.rest_url + '/generateToken',
+                data={
+                    'username': self.username,
+                    'password': self.password,
+                    'referer': self.url,
+                    'f': 'json'
+                }
+            ).json()
+            self._token = token_data['token']
+            self.token_expiration = datetime.datetime.utcfromtimestamp(token_data['expires'] / 1000)
+
+
+class PortalGIS(_GIS):
+    '''Connection to ArcGIS Enterprise Portal'''
+    def __init__(self, url, client_id, refresh_token):
+        super().__init__(url)
+        self.client_id = client_id
+        self.refresh_token = refresh_token
+        self.username = None
+
+    def _get_token(self):
+        if not self._token or \
+            datetime.datetime.utcnow() > self.token_expiration - datetime.timedelta(minutes=2):
             token_data = requests.get(
                 self.rest_url + '/oauth2/token',
                 params={
                     'grant_type': 'refresh_token',
                     'client_id': self.client_id,
-                    'refresh_token': self.refresh_token
+                    'refresh_token': self.refresh_token,
+                    'f': 'json'
                 }
             ).json()
             self._token = token_data['access_token']
-            self.token_expiration = datetime.datetime.now() + datetime.timedelta(seconds=token_data['expires_in'])
-
-    @property
-    def token(self) -> str:
-        '''GIS access token'''
-        self._get_token()
-        return self._token
+            self.token_expiration = datetime.datetime.utcnow() + datetime.timedelta(seconds=token_data['expires_in'])
+            self.username = token_data['username']
