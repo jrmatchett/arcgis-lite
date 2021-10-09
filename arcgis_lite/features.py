7a5a880e3ec3f7d2e8f328e226f19696a70c9d0f
J. R. Matchett <6579155+jrmatchett@users.noreply.github.com>
2021-10-08 22:16:21 -0700

    Edit feature layer

--- arcgis_lite/features.py
+++ arcgis_lite/features.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 
 class FeatureLayer:
@@ -13,7 +14,7 @@ class FeatureLayer:
         if kwargs:
             query_params.update(kwargs)
         query_params['f'] = 'json'
-        query_params['token'] = self.gis.token if self.gis else None
+        query_params['token'] = self.token
         #print(query_params, flush=True)
         return self._paged_query(query_params, [])
 
@@ -30,3 +31,41 @@ class FeatureLayer:
         if 'exceededTransferLimit' in query_data and query_data['exceededTransferLimit']:
             self._paged_query(query_params, features)
         return features
+
+    def add_features(self, features):
+        add_result = requests.post(
+            self.url + '/addFeatures',
+            data={
+                'features': json.dumps(features),
+                'f': 'json',
+                'token': self.token
+            }
+        ).json()
+        return add_result
+
+    def update_features(self, features):
+        update_result = requests.post(
+            self.url + '/updateFeatures',
+            data={
+                'features': json.dumps(features),
+                'f': 'json',
+                'token': self.token
+            }
+        ).json()
+        return update_result
+
+    def delete_features(self, where=None, objectIds=None):
+        delete_result = requests.post(
+            self.url + '/deleteFeatures',
+            data={
+                'where': where,
+                'objectIds': objectIds,
+                'f': 'json',
+                'token': self.token
+            }
+        ).json()
+        return delete_result
+
+    @property
+    def token(self):
+        return self.gis.token if self.gis else None
